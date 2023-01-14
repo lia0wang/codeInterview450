1ac09e3ed8efc896809ef6d61fc1391c230d65e7
lia0wang <lia0wang@outlook.com>
2023-01-15 01:28:41 +1100

    2/450

---
+++ array/find_max_min.py
@@ -0,0 +1,15 @@
+"""
+Input: arr[] = {3, 5, 4, 1, 9}
+Output: Minimum element is: 1
+              Maximum element is: 9
+
+Input: arr[] = {22, 14, 8, 17, 35, 3}
+Output:  Minimum element is: 3
+              Maximum element is: 35
+"""
+
+arr = [3, 5, 4, 1, 9]
+print(min(arr))
+print(max(arr))
+
+
